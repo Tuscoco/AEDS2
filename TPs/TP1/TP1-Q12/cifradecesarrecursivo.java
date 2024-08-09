929172e5b442d88d936efcd75edff4a9eeaea7e6
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2024-08-09 10:11:11 -0300

    Exercicios

---
+++ TPs/TP1/TP1-Q12/cifradecesarrecursivo.java
@@ -0,0 +1,71 @@
+/**
+ * cifradecesar
+ */
+
+ public class cifradecesarrecursivo {
+
+    public static int verificarIndex(char c){
+        int i;
+        int index = 0;
+
+        char[] alfabeto = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+        for(i = 0; i < alfabeto.length;i++){
+
+            if(c == alfabeto[i]){
+                index = i;
+                break;
+            }
+
+        }
+
+        return index;
+    }
+    
+    public static String criptografar(String str, int chave){
+        return criptografar(str, chave,0,new char[str.length()]);
+    }
+    
+    public static String criptografar(String str, int chave,int c,char[] resultadochar){
+        char[] strchar = str.toCharArray();
+        char[] alfabeto = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        String resultado = new String();
+        
+        if(c == str.length()){
+            
+            resultado = new String(resultadochar);
+
+        }else{
+
+            int index = verificarIndex(strchar[c]);
+
+            if(index + chave >= alfabeto.length){
+                int temp = index + chave;
+
+                temp -= alfabeto.length;
+
+                resultadochar[c] = alfabeto[temp];
+            }else{
+                resultadochar[c] = alfabeto[index + chave];
+            }
+
+            return criptografar(str, chave, c + 1,resultadochar);
+
+        }
+
+        return resultado;
+    }
+    
+    public static void main(String[] args) {
+        
+        int chave = 3;
+
+        String str = MyIO.readString();
+
+        String result = criptografar(str, chave);
+
+        System.out.println(result);
+
+    }
+
+}
