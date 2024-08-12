5343be292ea9c6399431e3218255cdb2732e4fda
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2024-08-11 21:28:10 -0300

    Exercicios

---
+++ Exercicios/Beecrowd/AreaDireita/Main.java
@@ -0,0 +1,64 @@
+package Exercicios.Beecrowd.AreaDireita;
+import java.util.*;
+
+public class Main {
+ 
+    public static double soma(double[][] matriz){
+
+        double resultado = 0;
+        int c = 1;
+
+        for(int i = 11;i > 6;i--,c++){
+
+            for(int j = c;j <= 11 - c;j++){
+
+                resultado += matriz[j][i];
+
+            }
+
+        }
+
+        return resultado;
+
+    }
+    
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        char o = scan.next().charAt(0);
+
+        double[][] matriz = new double[12][12];
+
+
+        for(int i = 0;i < 12;i++){
+
+            for(int j = 0;j < 12;j++){
+
+                double temp = scan.nextDouble();
+                
+                matriz[i][j] = temp;
+
+            }
+
+        }
+
+        double resultado;
+
+        if(o == 'S'){
+
+            resultado = soma(matriz);
+
+        }else{
+
+            resultado = soma(matriz)/30;
+
+        }
+
+        System.out.printf("%.1f\n",resultado);
+
+        scan.close();
+
+    }
+
+}
