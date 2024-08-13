4cf12ede0c89105daaa7f996b3a44eedf9c0c869
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2024-08-13 14:42:31 -0300

    TP1

--- TPs/TP1/TP1-Q3/cifradecesar.java
+++ TPs/TP1/TP1-Q3/cifradecesar.java
@@ -1,62 +1,84 @@
+import java.util.*;
+
 /**
  * cifradecesar
  */
 
 public class cifradecesar {
-
-    public static int verificarIndex(char c){
-        int i;
-        int index = 0;
-
-        char[] alfabeto = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-
-        for(i = 0; i < alfabeto.length;i++){
-
-            if(c == alfabeto[i]){
-                index = i;
-                break;
-            }
-
-        }
-
-        return index;
-    }
     
     public static String criptografar(String str, int chave){
-        char[] strchar = str.toCharArray();
-        char[] resultadochar = new char[str.length()];
-        char[] alfabeto = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
         int i = 0;
+        char[] resultadochar = str.toCharArray();
 
         for(i = 0;i < str.length();i++){
 
-            int index = verificarIndex(strchar[i]);
+            int letra = str.charAt(i);
 
-            if(index + chave >= alfabeto.length){
-                int temp = index + chave;
+            if(letra + chave > 126){
 
-                temp -= alfabeto.length;
+                if(str.charAt(i) == '�'){
 
-                resultadochar[i] = alfabeto[temp];
+                    resultadochar[i] = (char) (letra + chave);
+
+                }else{
+
+                    int temp = letra + chave;
+
+                    temp -= 94;
+    
+                    resultadochar[i] = (char) temp;
+
+                }
+
             }else{
-                resultadochar[i] = alfabeto[index + chave];
+
+                resultadochar[i] = (char) (letra + chave);
+
             }
 
         }
 
         String resultado = new String(resultadochar);
         return resultado;
+
+    }
+
+    public static String criptografar(String str){
+        return criptografar(str, 3);
+    }
+
+    public static boolean fim(String str){
+
+        boolean resultado = false;
+
+        if(str.equals("FIM")){
+            resultado = true;
+        }
+
+        return resultado;
+
     }
     
     public static void main(String[] args) {
-        
-        int chave = 3;
 
-        String str = MyIO.readString();
+        String str = "START";
+        Scanner scan = new Scanner(System.in);
 
-        String result = criptografar(str, chave);
+        do{
 
-        System.out.println(result);
+            //str = MyIO.readLine();
+            str = scan.nextLine();
+
+            if(!fim(str)){
+
+                System.out.println(criptografar(str));
+
+            }
+
+        }while(!fim(str));
+
+        scan.close();
 
     }
 
