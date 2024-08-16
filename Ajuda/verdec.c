91a85248fe36a71ecb0faf4f307c0a79acf9dbe8
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2024-08-15 22:28:25 -0300

    Testes

--- Ajuda/verdec.c
+++ Ajuda/verdec.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-
+    
+    char comando[256];
+    
     system("clear");
+    
+    snprintf(comando, sizeof(comando), "gcc %s -o exec", argv[1]);
+    system(comando);
+
     system("./exec < pub.in > saida.out");
+    
     system("diff pub.out saida.out");
 
     return 0;
